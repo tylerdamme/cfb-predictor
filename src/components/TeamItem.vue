f732483c4ce9c550dddb3d18d67a6d34626fffd4
Tyler Damme <tyler.damme@gmail.com>
2020-04-13 15:16:26 -0500

    accordion implementation, bootstrap added to project

--- src/components/TeamItem.vue
+++ src/components/TeamItem.vue
@@ -1,7 +1,18 @@
 <template>
-    <ul>
-        <li class="teamDisplay" :style="{backgroundColor: team.color}">{{team.school}} {{team.mascot}}</li>
-    </ul>
+    <div id="accordion">
+        <div class="card">
+            <div class="card-header" :style="{backgroundColor: team.color}">
+                <a class="card-link"  data-toggle="collapse" v-bind:href="'#collapse' + team.id">
+                    {{team.school}} {{team.mascot}}
+                </a>
+            </div>
+            <div v-bind:id="'collapse' + team.id" class="collapse show" data-parent="#accordion">
+            <div class="card-body">
+                The games will go here
+            </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,19 +22,16 @@ export default {
 }
 
 </script>
-
+<ul>
+        <li class="teamDisplay" :style="{backgroundColor: team.color}">{{team.school}} {{team.mascot}}</li>
+    </ul>
 <style>
     ul {
         list-style-type: none;
         vertical-align: middle;
     }
     
-    .teamDisplay {
-        border: 1px;
-        border-style: solid;
-        border-color: black;
-        width: 40%;
-        margin: auto;
+    .card-link {
         color: white;
     }
 </style>
